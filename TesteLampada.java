1d9a64ca723596048c47a141edf78e943c631585
ikatahira <isaque.katahira@fatec.sp.gov.br>
2021-11-11 17:28:35 +0000

    Lâmpada

---
+++ TesteLampada.java
@@ -0,0 +1,16 @@
+public class TesteLampada {
+    
+public static void main(String[] args) {
+    Lampada l=new Lampada();
+
+    l.ligar();
+
+    if(l.isSituacaoLampada()){
+        System.out.println("Lampada ligada");
+    }
+    else{
+        System.out.println("Lampada desligada");
+    }
+
+}
+}
